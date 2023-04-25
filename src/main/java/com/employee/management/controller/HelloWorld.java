f309b5a280182f16e054342a59c7ccc8cd2d021e
Reddy1311 <dhananjayareddy1311@gmail.com>
2023-04-25 08:10:23 +0530

    first commit

---
+++ src/main/java/com/employee/management/controller/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * @author shant
+ * HelloWorld Demo for LBG colleagues
+ */
+package com.employee.management.controller;
+
+public class HelloWorld {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 0; i < 10; i++) {
+			if(i == 0)
+				System.out.println("Hello World!!! Printed the " + (i+1) + " time");
+			else
+				System.out.println("Hello World!!! Printed the " + (i+1) + " times");
+		}
+	}
+
+}
